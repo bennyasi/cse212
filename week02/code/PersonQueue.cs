360c0a8520530f70fda4b7d0298e21381a66825f
bennyasi <bennyasi64@gmail.com>
2025-07-16 23:54:13 +0100

    Update PersonQueue.cs

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,24 +1,23 @@
-using System;
-using System.Collections.Generic;
-
+/// <summary>
+/// A basic implementation of a Queue
+/// </summary>
 public class PersonQueue
 {
     private readonly List<Person> _queue = new();
 
     public int Length => _queue.Count;
 
+    /// <summary>
+    /// Add a person to the queue
+    /// </summary>
+    /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Add(person);
+        _queue.Insert(Length, person);
     }
 
     public Person Dequeue()
     {
-        if (_queue.Count == 0)
-        {
-            throw new InvalidOperationException("Queue is empty.");
-        }
-
         var person = _queue[0];
         _queue.RemoveAt(0);
         return person;
@@ -26,7 +25,7 @@ public class PersonQueue
 
     public bool IsEmpty()
     {
-        return _queue.Count == 0;
+        return Length == 0;
     }
 
     public override string ToString()
