212ce989c3a026ff3a0e0cbf3051859df1182413
bennyasi <bennyasi64@gmail.com>
2025-07-17 11:32:16 +0100

    Update Arrays_Tests.cs

--- week01/code/Arrays_Tests.cs
+++ week01/code/Arrays_Tests.cs
@@ -1,28 +1,64 @@
-using System.Collections.Generic;
-using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-public class Arrays_Tests
+// DO NOT MODIFY THIS FILE
+
+[TestClass]
+public class MultiplesOfTests
 {
-    [Fact]
-    public void MultiplesOf_ReturnsCorrectArray()
+    [TestMethod]
+    public void TestMultiplesOf_Whole()
     {
-        var result = Arrays.MultiplesOf(3, 5);
-        Assert.Equal(new double[] { 3, 6, 9, 12, 15 }, result);
+        double[] multiples = Arrays.MultiplesOf(7, 5);
+        CollectionAssert.AreEqual(new double[] { 7, 14, 21, 28, 35 }, multiples);
     }
 
-    [Fact]
-    public void RotateListRight_Amount3_ReturnsCorrectList()
+    [TestMethod]
+    public void TestMultiplesOf_Fractional()
     {
-        var input = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        Arrays.RotateListRight(input, 3); // No assignment
-        Assert.Equal(new List<int> { 7, 8, 9, 1, 2, 3, 4, 5, 6 }, input);
+        double[] multiples = Arrays.MultiplesOf(1.5, 10);
+        CollectionAssert.AreEqual(new double[] { 1.5, 3.0, 4.5, 6.0, 7.5, 9.0, 10.5, 12.0, 13.5, 15.0 }, multiples);
     }
 
-    [Fact]
-    public void RotateListRight_Amount5_ReturnsCorrectList()
+    [TestMethod]
+    public void TestMultiplesOf_Negative()
     {
-        var input = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        Arrays.RotateListRight(input, 5); // No assignment
-        Assert.Equal(new List<int> { 5, 6, 7, 8, 9, 1, 2, 3, 4 }, input);
+        double[] multiples = Arrays.MultiplesOf(-2, 10);
+        CollectionAssert.AreEqual(new double[] { -2, -4, -6, -8, -10, -12, -14, -16, -18, -20 }, multiples);
+    }
+}
+
+[TestClass]
+public class RotateListRightTests
+{
+    [TestMethod]
+    public void TestRotateListRight_Rotate1()
+    {
+        List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Arrays.RotateListRight(numbers, 1);
+        CollectionAssert.AreEqual(new List<int> { 9, 1, 2, 3, 4, 5, 6, 7, 8 }, numbers);
+    }
+
+    [TestMethod]
+    public void TestRotateListRight_Rotate5()
+    {
+        List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Arrays.RotateListRight(numbers, 5);
+        CollectionAssert.AreEqual(new List<int> { 5, 6, 7, 8, 9, 1, 2, 3, 4 }, numbers);
+    }
+
+    [TestMethod]
+    public void TestRotateListRight_Rotate3()
+    {
+        List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Arrays.RotateListRight(numbers, 3);
+        CollectionAssert.AreEqual(new List<int> { 7, 8, 9, 1, 2, 3, 4, 5, 6 }, numbers);
+    }
+
+    [TestMethod]
+    public void TestRotateListRight_Rotate9()
+    {
+        List<int> numbers = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Arrays.RotateListRight(numbers, 9);
+        CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, numbers);
     }
 }
